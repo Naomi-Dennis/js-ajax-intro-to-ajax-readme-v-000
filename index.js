dcb03eb4dea44e246890cb06ed364b77d3c85246
Naomi Dennis <njdennis94@gmail.com>
2018-11-06 20:03:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function getRepositories() {
   const req = new XMLHttpRequest();
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
+  showRepositories();
 }
 
 function showRepositories() {
